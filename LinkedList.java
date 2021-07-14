b8779a51ab2d22ce09412d1fcf123a98bf83beb4
FawadZahidSystems <87422875+FawadZahidSystems@users.noreply.github.com>
2021-07-14 16:59:52 +0500

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -39,6 +39,9 @@ class LinkedList<T>
         // If element to remove is the head
 		if (current != null && current.data == data) {
 			head = current.next;
+			current = null;
+			//Decrease node count
+			nodes--;
 		}
 
         // Traverse through the LinkedList until data is found
@@ -50,9 +53,9 @@ class LinkedList<T>
 		//Remove value
 		if (current != null) {
 			prev.next = current.next;
+			//Decrease node count
+			nodes--;
 		}
-		//Decrease node count
-		nodes--;
 	}
 	
     public void printList()
@@ -64,5 +67,62 @@ class LinkedList<T>
             // get next node
             current = current.next;
         }
+    }
+    
+    public void replace(T oldData, T newData)
+    {
+        Node<T> current = head;
+        // Traverse through the LinkedList until old data is found
+        while(current!= null && current.data != oldData)
+        	current = current.next;
+        //override old data with new data
+        if (current != null)
+        	current.data = newData;
+    }
+    
+    public void insertAfter(int index, T data)
+    {
+    	//call add method if index >= nodes - 1 as element is to be added in the end of the list
+    	if (index >= nodes)
+    	{
+    		add(data);
+    		return;
+    	}
+    	
+        Node<T> current = head;
+        Node<T> next = current.next;
+        
+        // Traverse through the LinkedList so that current points to the indexed element
+        for(int i = 0; i < index; i++)
+        {
+        	current = current.next;
+        	next = current.next;
+        }
+        
+        // add the new element between current and next
+        Node<T> newNode = new Node<T>(data);
+        current.next = newNode;
+        newNode.next = next;
+        nodes++;
+    }
+    
+    public T elementAt(int index)
+    {
+    	//return tail data if index is of last element or more
+    	if (index >= nodes - 1)
+    		return tail.data;
+    	
+        Node<T> current = head;
+        // Traverse through the LinkedList so that current points to the indexed element
+        for(int i = 0; i < index; i++)
+        	current = current.next;
+        
+        return current.data;
+    }
+    
+
+    public int nodeCount()
+    {
+    	return nodes;
     }
 }
